186caab9226f8542c926750608739534917b7db0
AmitShankerPandey2102 <amitshanker05@gmail.com>
2026-02-15 22:29:20 +0530

    Time: 1 ms (95.99%) | Memory: 47.2 MB (54.74%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -4,8 +4,8 @@
         int[] ans = new int[2 * n];
         
         for (int i = 0; i < n; i++) {
-            ans[i] = nums[i];
-            ans[i + n] = nums[i];
+            ans[i] = nums[i];                       // copy the element for the first half 
+            ans[i + n] = nums[i];                   // copy the element for the second half for concatenation.
         }
         
         return ans;
