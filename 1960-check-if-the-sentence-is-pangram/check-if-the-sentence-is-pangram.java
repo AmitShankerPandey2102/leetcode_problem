808cf5577754e7a406075a6d9e7f79bd04bd6ba9
AmitShankerPandey2102 <amitshanker05@gmail.com>
2026-02-25 12:48:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (85.25%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        if(sentence.length()<26) return false;
+        if(sentence.length()<26) return false; // pangram me 26 elemnt hone he chayie agar 26 se kaam hai tho false ;
         for(char ch='a';ch<='z';ch++){
             if(sentence.indexOf(ch)<0){
                 return false;
@@ -9,3 +9,5 @@ class Solution {
         return true;
     }
 }
+ // if me jo likha hai wah manually har lowercase check kar raha agar char mil raha tho index return kar raha hai aur nahi tho turant false .
+ // agar loop complete ho gaya aur koi miss nahi hua tho true return karega .
