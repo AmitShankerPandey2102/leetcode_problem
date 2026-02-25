6aad884c4340a7e7e13b7ab5294c8de538a51b66
AmitShankerPandey2102 <amitshanker05@gmail.com>
2026-02-25 12:44:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (53.84%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,21 +1,11 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-
-        boolean[] seen = new boolean[26];   // a-z track karne ke liye
-        int count = 0;
-
-        for (int i = 0; i < sentence.length(); i++) {
-            char ch = sentence.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {
-                int index = ch - 'a';
-                if (!seen[index]) {
-                    seen[index] = true;
-                    count++;
-                }
+        if(sentence.length()<26) return false;
+        for(char ch='a';ch<='z';ch++){
+            if(sentence.indexOf(ch)<0){
+                return false;
             }
-        }
-
-        return count == 26;
+         }
+        return true;
     }
 }
