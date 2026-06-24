c630846a59279797cf0acae31961146082349763
AmitShankerPandey2102 <amitshanker05@gmail.com>
2026-06-24 09:00:01 +0530

    Time: 3 ms (78.15%) | Memory: 46.5 MB (49.41%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,23 +1,94 @@
 class Solution {
     public int romanToInt(String s) {
-        int res = 0;
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
-                res -= roman.get(s.charAt(i));
-            } else {
-                res += roman.get(s.charAt(i));
+        int ans=0;
+        int n=s.length();
+        char[] arr = s.toCharArray();
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]=='I'){
+                ans+=1;
+            }
+            else if(arr[i]=='V'){
+                if(i==0){
+                    ans+=5;
+                }
+                else if(arr[i-1]=='I'){
+                    ans+=5;
+                    ans-=1;
+                    i--;
+                }
+                else{
+                    ans+=5;
+                }
+            }
+            else if(arr[i]=='X'){
+                if(i==0){
+                    ans+=10;
+                }
+                else if(arr[i-1]=='I'){
+                    ans+=10;
+                    ans-=1;
+                    i--;
+                }
+                else{
+                    ans+=10;
+                }
+            }
+            else if(arr[i]=='L'){
+                if(i==0){
+                    ans+=50;
+                }
+                else if(arr[i-1]=='X'){
+                    ans+=50;
+                    ans-=10;
+                    i--;
+                }
+                else{
+                    ans+=50;
+                }
+            }
+            else if(arr[i]=='C'){
+                if(i==0){
+                    ans+=100;
+                }
+                else if(arr[i-1]=='X'){
+                    ans+=100;
+                    ans-=10;
+                    i--;
+                }
+                else{
+                    ans+=100;
+                }
+            }
+            else if(arr[i]=='D'){
+                if(i==0){
+                    ans+=500;
+                }
+                else if(arr[i-1]=='C'){
+                    ans+=500;
+                    ans-=100;
+                    i--;
+                }
+                else{
+                    ans+=500;
+                }
+            }
+            else if(arr[i]=='M'){
+                if(i==0){
+                    ans+=1000;
+                }
+                else if(arr[i-1]=='C'){
+                    ans+=1000;
+                    ans-=100;
+                    i--;
+                }
+                else{
+                    ans+=1000;
+                }
+            }
+            else{
+                continue;
             }
         }
-
-        return res + roman.get(s.charAt(s.length() - 1));        
+        return ans;
     }
 }
