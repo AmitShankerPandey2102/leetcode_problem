9d77a933a61889e6c805c5164ee9a3bb5182bc51
AmitShankerPandey2102 <amitshanker05@gmail.com>
2026-07-01 20:26:13 +0530

    Time: 2 ms (94.29%) | Memory: 47 MB (71.90%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,38 +1,27 @@
 class Solution {
-    public List<Integer> luckyNumbers(int[][] matrix) 
-    {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        List<Integer> row=new ArrayList<>();
-        List<Integer> col=new ArrayList<>();
-        for(int i=0;i<m;i++)
-        {
-            int minimum=Integer.MAX_VALUE;
-            for(int j=0;j<n;j++)
-            {
-                minimum=Math.min(matrix[i][j],minimum);
-            }
-            row.add(minimum);
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+         int max = Integer.MIN_VALUE;
+         List<Integer> l = new ArrayList<>();
+        for(int i=0;i<n;i++){
+             int min = Integer.MAX_VALUE;
+             int minidx=-1;
+             for(int j=0;j<m;j++){
+                if(min>matrix[i][j]){
+                    min = matrix[i][j];
+                    minidx = j;
+                }
+             } 
+              int max1=0;
+             for(int k=0;k<n;k++){
+                   max1 = Math.max(max1,matrix[k][minidx]);
+                }
+              if(max1==min){
+               if(l.size()==0) l.add(max1);
+              }  
         }
-        for(int i=0;i<n;i++)
-        {
-            int maximum=Integer.MIN_VALUE;
-            for(int j=0;j<m;j++)
-            {
-                maximum=Math.max(matrix[j][i],maximum);
-            }
-            col.add(maximum);
-        }
-        List<Integer> res=new ArrayList<>();
-        for(int num:row)
-        {
-            if(col.contains(num))
-            {
-                res.add(num);
-            }
-        }
-        return res;
-        
+        return l;
         
     }
 }
