72a672d9471c8e165cda1d6174c1d3341830c91f
AmitShankerPandey2102 <amitshanker05@gmail.com>
2026-09-22 03:36:45 +0530

    Time: 4 ms (47.20%) | Memory: 44.6 MB (89.75%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        int[] result = new int[Math.min(nums1.length, nums2.length)];
+        boolean[] used = new boolean[nums2.length];
+
+        int index = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            for (int j = 0; j < nums2.length; j++) {
+
+                if (nums1[i] == nums2[j] && !used[j]) {
+
+                    result[index] = nums1[i];
+                    index++;
+
+                    used[j] = true;
+
+                    break;
+                }
+            }
+        }
+
+        return Arrays.copyOf(result, index);
+    }
+}
